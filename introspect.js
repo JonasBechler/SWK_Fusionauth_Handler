39fefc1c57da08606acf3a6c54a63b84b842d3ef
Jonas Bechler <jonasbechler@gmail.com>
2023-01-03 09:46:13 +0100

    handling edge cases and connect accounts

--- introspect.js
+++ introspect.js
@@ -1,25 +1,24 @@
 module.exports = function(config){
-    const request = require('request');
+	
+	const util = require('util')
+	const request = require("request");
+	
+	const requestPromise = util.promisify(request);
 
-    return function introspect (token, callback){
-        request(
-            // POST request to /introspect endpoint
-            {
-              method: 'POST',
-              uri: `${config.device_ip}:${config.fusionauth_port}/oauth2/introspect`,
-              form: {
-                'client_id': config.fusionauth.client_id,
-                'token': token
-              }
-            },
-    
-            // callback
-            (error, response, body) => {
-              let introspectResponse = JSON.parse(body);
-    
-              // valid token -> get more user data and send it back to the react app
-              callback(introspectResponse)
-            }
-        );
-    }
+	return function introspect (token){
+		return requestPromise(
+			// POST request to /introspect endpoint
+			{
+				method: 'POST',
+				uri: `${config.device_ip}:${config.fusionauth_port}/oauth2/introspect`,
+				form: {
+					'client_id': config.fusionauth.client_id,
+					'token': token
+				}
+			}
+		)
+		.then(response => {
+			return JSON.parse(response.body);
+		})
+	}
 }
--- login.js
+++ login.js
@@ -1,53 +1,53 @@
 module.exports = function(config){
-    
-    const pkce = require('../helpers/pkce');
-    const request = require('request');
+	
+	const pkce = require('../helpers/pkce');
+
+	const util = require('util')
+	const request = require('request');
+	const requestPromise = util.promisify(request);
 
 
-    function redirect (req, res){
-        // Generate and store the PKCE verifier
-        req.session.verifier = pkce.generateVerifier();
 
-        // Generate the PKCE challenge
-        const challenge = pkce.generateChallenge(req.session.verifier);
+	function redirect (req, res){
+		// Generate and store the PKCE verifier
+		req.session.verifier = pkce.generateVerifier();
 
-        // Redirect the user to log in via FusionAuth
-        const redirect_uri = `${config.device_ip}:${config.fusionauth_port}/oauth2/authorize?client_id=${config.fusionauth.client_id}&redirect_uri=${config.device_ip}:${config.port}${config.fusionauth.redirect_uri}&response_type=code&code_challenge=${challenge}&code_challenge_method=S256`
-        res.redirect(redirect_uri);
-    }
+		// Generate the PKCE challenge
+		const challenge = pkce.generateChallenge(req.session.verifier);
 
-    function callback (req, res){
-        request(
-            // POST request to /token endpoint
-            {
-              method: 'POST',
-              uri: `${config.device_ip}:${config.fusionauth_port}/oauth2/token`,
-              form: {
-                'client_id': config.fusionauth.client_id,
-                'client_secret': config.fusionauth.client_secret,
-                'code': req.query.code,
-                'code_verifier': req.session.verifier,
-                'grant_type': 'authorization_code',
-                'redirect_uri': `${config.device_ip}:${config.port}${config.fusionauth.redirect_uri}`
-              }
-            },
-      
-            // callback
-            (error, response, body) => {
-                let data = JSON.parse(body)
-                console.log(data)
+		// Redirect the user to log in via FusionAuth
+		const redirect_uri = `${config.device_ip}:${config.fusionauth_port}/oauth2/authorize?client_id=${config.fusionauth.client_id}&redirect_uri=${config.device_ip}:${config.port}${config.fusionauth.redirect_uri}&response_type=code&code_challenge=${challenge}&code_challenge_method=S256`
+		res.redirect(redirect_uri);
+	}
 
-                // save token to session
-                req.session.token = data.access_token;
-                
-                // redirect to the React app
-                res.redirect(`${config.device_ip}:${config.port_react}`);
-      
-            }
-        );
-    }
+	function callback (req, res){
+		requestPromise(
+			// POST request to /token endpoint
+			{
+			  method: 'POST',
+			  uri: `${config.device_ip}:${config.fusionauth_port}/oauth2/token`,
+			  form: {
+				'client_id': config.fusionauth.client_id,
+				'client_secret': config.fusionauth.client_secret,
+				'code': req.query.code,
+				'code_verifier': req.session.verifier,
+				'grant_type': 'authorization_code',
+				'redirect_uri': `${config.device_ip}:${config.port}${config.fusionauth.redirect_uri}`
+			  }
+			}
+		)
+		.then(response => {
+			let data = JSON.parse(response.body)
 
-    return {
-        redirect: redirect,
-        callback: callback}
+			// save token to session
+			req.session.token = data.access_token;
+				
+			// redirect to the React app
+			res.redirect(`${config.device_ip}:${config.port_react}`);
+		})
+	}
+
+	return {
+		redirect: redirect,
+		callback: callback}
 }
