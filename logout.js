5958b3487d9102ed0cd0bde8ca71aba2bd173adb
JonasBechler <jonasbechler@gmail.com>
2023-02-23 11:14:24 +0100

    update logout link

--- logout.js
+++ logout.js
@@ -6,12 +6,16 @@ module.exports = function(config){
 // Logout the user via FusionAuth
 
 function logout (req, res){
-    const fa_address = `${config.device_ip}:${config.fusionauth_port}`
+    const fa_address = `${config.device_ip}:${config.fusionauth_port}`;
 
     // Redirect the user to logout via FusionAuth 
     // and then back to in FusionAuth specified logout URL.
+    const url 					= `${fa_address}/oauth2/logout?`;
+    const client_id 			= `client_id=${config.fusionauth.client_id}`;
 
-    res.redirect(`${fa_address}/oauth2/logout?client_id=${config.fusionauth.client_id}`);
+    const redirect_uri = url + client_id;
+
+    res.redirect(redirect_uri);
 }
 
 // ####################################################################################################################
